4c5570ff225d3bd2d13a3f31b6ce6906cc806cf2
Daniel Wayne Armstrong <daniel@circuidipity.com>
2017-04-22 21:35:33 -0400

    save

--- grubs.sh
+++ grubs.sh
@@ -14,30 +14,143 @@ SOURCE="https://github.com/vonbrownie/grubs"
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 # or FITNESS FOR A PARTICULAR PURPOSE. See the LICENSE file for more details.
 
-USB_DEVICE="${@: -1}"
+# Import some helpful functions
+. ./Library.sh
 
-# ANSI escape codes
-RED="\033[1;31m"
-YELLOW="\033[1;33m"
-NC="\033[0m" # no colour
+# Removable USB storage device 'sd[b-z]1'
+USB_DEVICE="${*: -1}"
 
-echo_red() {
-echo -e "${RED}$1${NC}"
+
+test_usb_device() {
+ERR0="ERROR: script requires the USB_DEVICE_PARTITION argument."
+ERR1="ERROR: '$USB_DEVICE' not available for use."
+FIX0="FIX: run script with a (valid) DEVICE as 'grubs.sh sd[b-z]1'."
+if [[ -z "$USB_DEVICE" ]]
+then
+    L_echo_red "\n$( L_penguin ) .: $ERR0"
+    L_echo_red "$FIX0"
+    exit 1
+fi
+if [[ ! -b /dev/$USB_DEVICE ]] || [[ ! $USB_DEVICE == sd[b-z]1 ]]
+then
+    echo ""
+    L_echo_red "$( L_penguin ) .: $ERR1"
+    L_echo_red "$FIX0"
+    exit 1
+fi
 }
 
-echo_yellow() {
-echo -e "${YELLOW}$1${NC}"
+
+create_warning() {
+L_echo_red "\n\n\t\t### WARNING ###"
+L_echo_red "Make careful note of the drive partition labels on your system!\n"
+L_echo_red "INSTALL option will **destroy all data** currently stored on the"
+L_echo_red "chosen partition **$USB_DEVICE**.\n"
+while :
+do
+    read -n 1 -p "Proceed with INSTALL? [yN] > "
+    if [[ $REPLY == [yY] ]]
+    then
+        break
+    elif [[ $REPLY == [nN] || $REPLY == "" ]]
+    then
+        echo -e "\n$( L_penguin )"
+        exit
+    else
+        L_invalid_reply_yn
+    fi
+done
 }
 
-penguin() {
-cat << _EOF_
-(O<
-(/)_
-_EOF_
+
+#
+# L_mktemp_dir_pwd
+#
+
+
+detect_mnt() {
+mount | grep "/dev/$USB_DEVICE" | cut -d' ' -f1-3
 }
+
+
+usb_mnt() {
+if [[ $( detect_mnt ) ]]
+then
+    echo "Mounted partition $( detect_mnt ) detected."
+else
+    echo "Mounting partition $USB_DEVICE on mntpoint ..."
+    echo "sudo mount /dev/$USB_DEVICE mntpoint"
+fi
+}
+
+usb_umnt() {
+    :
+}
+
+
+format_partition() {
+TASK="FORMAT PARTITION"
+L_banner_begin "$TASK"
+echo "Creating vfat filesystem on $USB_DEVICE ..."
+if [[ $( detect_mnt ) ]]
+then
+    echo "Unmounting $( detect_mnt ) ..."
+    echo "sudo umount /dev/$USB_DEVICE"
+else
+    echo "Partition $USB_DEVICE is not mounted."
+fi
+echo "sudo mkfs.vfat -n MULTIBOOT /dev/$USB_DEVICE"
+L_banner_end "$TASK"
+}
+
+
+make_bootdir() {
+TASK="MAKE BOOTDIR"
+L_banner_begin "$TASK"
+echo "Creating a boot folder for GRUB files and Linux ISO images ..."
+echo "sudo mkdir -p /media/MOUNTPOINT/boot/{grub,iso,debian}"
+L_banner_end "$TASK"
+}
+
+
+grub_install() {
+TASK="GRUB INSTALL"
+L_banner_begin "$TASK"
+echo "Install GRUB to the Master Boot Record (MBR) of $USB_DEVICE ..."
+echo "sudo grub-install --force --no-floppy --root-directory=/media/MOUNTPOINT /dev/sdX"
+L_banner_end "$TASK"
+}
+
+
+sync_bootdir() {
+TASK="SYNC BOOTDIR"
+L_banner_begin "$TASK"
+echo "Sync GRUBS boot with /mntpoint/$USB_DEVICE/boot ..."
+echo "rsync --->"
+L_banner_end "$TASK"
+}
+
+
+run_options() {
+while getopts ":h" OPT
+do
+    case $OPT in
+        h)
+            greeting
+            exit
+            ;;
+        ?)
+            greeting
+            L_echo_red "\n$( L_penguin ) .: ERROR: Invalid option '-$OPTARG'"
+            exit 1
+            ;;
+    esac
+done
+}
+
 
 greeting() {
-echo -e "\n$( penguin ) .: Howdy!"
+echo -e "\n$( L_penguin ) .: Howdy!"
 cat << _EOF_
 NAME
     $NAME
@@ -57,273 +170,25 @@ DESCRIPTION
     More info: http://www.circuidipity.com/multi-boot-usb.html
 SOURCE
     $SOURCE
+
 _EOF_
-echo ""
 }
 
-run_options() {
-while getopts ":h" OPT
-do
-    case $OPT in
-        h)
-            greeting
-            exit
-            ;;
-        ?)
-            greeting
-            echo_red "\n$( penguin ) .: ERROR: Invalid option '-$OPTARG'"
-            exit 1
-            ;;
-    esac
-done
-}
-
-invalid_reply() {
-echo_red "\n'${REPLY}' is invalid input..."
-}
-
-invalid_reply_yn() {
-echo_red "\n'${REPLY}' is invalid input. Please select 'Y(es)' or 'N(o)'..."
-}
-
-test_usb_device() {
-local err_mesg0
-local err_mesg1
-local fix_mesg0
-err_mesg0="ERROR: script requires the USB_DEVICE_PARTITION argument."
-err_mesg1="ERROR: '$USB_DEVICE' not available for use."
-fix_mesg0="FIX: run script with a (valid) DEVICE as 'grubs.sh sd[b-z]1'."
-if [[ -z "$USB_DEVICE" ]]
-then
-    echo_red "\n$( penguin ) .: $err_mesg0"
-    echo_red "$fix_mesg0"
-    exit 1
-fi
-if [[ ! -b /dev/$USB_DEVICE ]] || [[ ! $USB_DEVICE == sd[b-z]1 ]]
-then
-    echo ""
-    echo_red "$( penguin ) .: $err_mesg1"
-    echo_red "$fix_mesg0"
-    exit 1
-fi
-}
 
 go_no_go() {
 test_usb_device
-echo_yellow "\nYou have chosen **$USB_DEVICE** as USB_DEVICE_PARTITION.\n"
-while :
-do
-    read -n 1 -p "Run script now? [yN] > "
-    if [[ $REPLY == [yY] ]]
-    then
-        echo -e "\nLet's roll then ..."
-        sleep 2
-        if [[ -x "/usr/games/sl" ]]
-        then
-            /usr/games/sl
-        fi
-        break
-    elif [[ $REPLY == [nN] || $REPLY == "" ]]
-    then
-        echo -e "\n$( penguin )"
-        exit
-    else
-        invalid_reply_yn
-    fi
-done
+L_echo_yellow "\nYou have chosen **$USB_DEVICE** as USB_DEVICE_PARTITION.\n"
+L_run_script
 }
 
-
-test_root() {
-if [[ $UID -ne 0 ]]
-then
-    echo_red "$( penguin ) .: ERROR: GRUBS requires ROOT privileges to do its job."
-    exit 1
-fi
-}
-
-test_mount() {
-## A test to ensure that our modification are
-## going to '/MOUNTPOINT/boot' NOT '/boot'
-local mountpoint
-mountpoint=$(mount | grep $USB_DEVICE | awk '{print $3}')
-if [[ "${mountpoint}/boot" == "/boot" ]]
-then
-    echo ""
-    echo_red "$( penguinista ) .: ERROR: DESTINATION is the root directory... Abort!"
-    exit 1
-fi
-}
-
-mountdir() {
-local multi_mnt
-multi_mnt="/mnt"
-if ! $(mount | grep /dev/${USB_DEVICE} >/dev/null)
-then
-    if $(mount | grep ${multi_mnt} >/dev/null)
-    then
-        echo ""
-        echo_red "$( penguinista ) .: ERROR: Mountpoint '${multi_mnt}' is not available for use."
-        echo "FIX: Please unmount any partitions attached to $multi_mnt and re-run ${GRUBS}."
-        exit 1
-    else
-        echo ""
-        echo "OK. Mounting /dev/${USB_DEVICE} to ${multi_mnt}..."
-        mount -t vfat /dev/${USB_DEVICE} $multi_mnt
-    fi
-fi
-}
-
-umountdir() {
-if $(mount | grep /dev/${USB_DEVICE} >/dev/null)
-then
-    echo ""
-    echo "OK... Unmounting /dev/${USB_DEVICE}..."
-    umount /dev/${USB_DEVICE}
-fi
-}
-
-createdir() {
-mountdir
-test_mount
-local mountpoint
-mountpoint=$(mount | grep $USB_DEVICE | awk '{print $3}')
-echo ""
-echo "Creating BOOT and ISO folders..."
-mkdir ${mountpoint}/{boot,iso}
-}
-
-create_filesystem() {
-while :
-do
-read -n 1 -p \
-    "### WARNING ### All data will be wiped from ${USB_DEVICE}! Proceed? [yN] > "
-if [[ $REPLY == [yY] ]]
-then
-    umountdir
-    echo ""
-    echo "Creating FAT32 filesystem on ${USB_DEVICE}..."
-    mkfs.vfat -n MULTIBOOT /dev/${USB_DEVICE}
-    echo ""
-    break
-elif [[ $REPLY == [nN] || $REPLY == "" ]]
-then
-    echo ""
-    echo "Abort install."
-    echo ""
-    echo_green "$( penguinista )"
-    exit
-else
-    invalid_reply_yn
-fi
-done
-}
-
-grub_mbr() {
-local mountpoint
-mountpoint=$(mount | grep $USB_DEVICE | awk '{print $3}')
-# Install GRUB to the Master Boot Record (MBR) of the USB USB_DEVICE
-echo ""
-echo "Installing GRUB to MBR of ${USB_DEVICE:0:3}..."
-grub-install --force --no-floppy --root-directory=$mountpoint /dev/${USB_DEVICE:0:3}
-echo ""
-}
-
-grub_cfg() {
-mountdir
-test_mount
-local mountpoint
-mountpoint=$(mount | grep $USB_DEVICE | awk '{print $3}')
-local device_grub_conf
-device_grub_conf="${mountpoint}/boot/grub/grub.cfg"
-echo ""
-echo "Copying grub.cfg..."
-if [[ -e "$device_grub_conf" ]]
-then
-    cp $device_grub_conf ${device_grub_conf}.$(date +%Y-%m-%dT%H%M%S).bak
-fi
-cp $grub_conf $device_grub_conf
-echo ""
-}
-
-linux_iso() {
-local mountpoint
-mountpoint=$(mount | grep $USB_DEVICE | awk '{print $3}')
-echo ""
-echo "Copying Linux *.iso files..."
-## See http://rsync.samba.org/FAQ.html#2 about using the "--modify-window=1"
-## option to better manage modification times when using rsync between Linux
-## and FAT32 filesystems
-rsync --recursive --update --delete --progress --modify-window=1 \
-    --include '*iso' --exclude '*' ${iso_dir}/ ${mountpoint}/iso/
-# Copy memtest<VERSION>.bin
-cp ${iso_dir}/*.bin ${mountpoint}/boot/
-sleep 15
-echo ""
-}
-
-detect_mnt() {
-mount | grep "/dev/$USB_DEVICE" | cut -d' ' -f1-3
-}
-
-format_partition() {
-#local detect_mnt
-#detect_mnt=$( mount | grep "/dev/$USB_DEVICE" | cut -d' ' -f1-3 )
-echo_yellow "\n\t\t*** FORMAT PARTITION BEGIN ***\n"
-#if [[ $detect_mnt ]]
-if [[ $( detect_mnt ) ]]
-then
-    echo "Unmounting $( detect_mnt ) ..."
-    echo "sudo umount /dev/$USB_DEVICE"
-else
-    echo "Partition $USB_DEVICE is not mounted."
-fi
-echo "Creating vfat filesystem on $USB_DEVICE ..."
-echo "sudo mkfs.vfat -n MULTIBOOT /dev/$USB_DEVICE"
-echo_yellow "\n\t\t*** FORMAT PARTITION END ***\n"
-}
-
-make_bootdir() {
-echo_yellow "\n\t\t*** MAKE BOOTDIR BEGIN ***\n"
-echo_yellow "\n\t\t*** MAKE BOOTDIR END ***\n"
-}
-
-grub_install() {
-echo_yellow "\n\t\t*** GRUB INSTALL BEGIN ***\n"
-echo_yellow "\n\t\t*** GRUB INSTALL END ***\n"
-}
-
-sync_bootdir() {
-echo_yellow "\n\t\t*** SYNC BOOTDIR BEGIN ***\n"
-echo_yellow "\n\t\t*** SYNC BOOTDIR END ***\n"
-}
-
-create_warning() {
-echo_red "\n\n\t\t### WARNING ###"
-echo_red "Make careful note of the drive and partition labels on your system!\n"
-echo_red "The INSTALL option will **destroy all data** currently stored on the"
-echo_red "chosen partition **$USB_DEVICE**.\n"
-while :
-do
-    read -n 1 -p "Proceed with INSTALL? [yN] > "
-    if [[ $REPLY == [yY] ]]
-    then
-        break
-    elif [[ $REPLY == [nN] || $REPLY == "" ]]
-    then
-        echo -e "\n$( penguin )"
-        exit
-    else
-        invalid_reply_yn
-    fi
-done
-}
 
 create_or_update() {
-st0="Create a FAT32 partition on $USB_DEVICE"
-st1="Create /boot/{grub,iso,debian} on $USB_DEVICE"
-st2="Install GRUB to the Master Boot Record (MBR) of $USB_DEVICE"
-st3="Sync GRUB config and ISO images from grubs/boot to /boot on $USB_DEVICE"
+TASK1="INSTALL"
+TASK2="UPDATE"
+ST0="Create a FAT32 partition on $USB_DEVICE"
+ST1="Create /boot/{grub,iso,debian} on $USB_DEVICE"
+ST2="Install GRUB to the Master Boot Record (MBR) of $USB_DEVICE"
+ST3="Sync GRUB config and ISO images from grubs/boot to /boot on $USB_DEVICE"
 while :
 do
 cat << _EOF_
@@ -339,47 +204,39 @@ read -n 1 -p "Your choice? [0-2] > "
 
 case $REPLY in
     0)  create_warning
-        echo_yellow "\n\n\t\t*** INSTALL BEGIN ***\n"
-        echo -e "Steps ...\n0) $st0\n1) $st1\n2) $st2\n3) $st3"
+        echo ""
+        L_banner_begin "$TASK1"
+        echo -e "Steps ...\n0) $ST0\n1) $ST1\n2) $ST2\n3) $ST3"
         format_partition
         make_bootdir
         grub_install
         sync_bootdir
-        echo_yellow "\n\t\t*** INSTALL END ***\n"
+        L_banner_end "$TASK1"
         sleep 2
         break
         ;;
-    1)  echo_yellow "\n\n\t\t*** UPDATE BEGIN ***\n"
-        echo -e "Steps ...\n0) $st3"
+    1)  echo ""
+        L_banner_begin "$TASK2"
+        echo -e "Steps ...\n0) $ST3"
         sync_bootdir
-        echo_yellow "\n\t\t*** UPDATE END ***\n"
+        L_banner_end "$TASK2"
         sleep 2
         break
         ;;
     2)  echo ""
-        penguin
+        L_penguin
         exit 0
         ;;
-    *)  invalid_reply
+    *)  L_invalid_reply
         ;;
 esac
 done
 }
 
-all_done() {
-local message
-message="All done!"
-if [[ -x "/usr/games/cowsay" ]]
-then
-    /usr/games/cowsay "$message"
-else
-    echo -e "$( penguin ) .: $message"
-fi
-}
 
 # START
-run_options "$@"
-greeting
-go_no_go
-create_or_update
-all_done
+#run_options "$@"
+#greeting
+#go_no_go
+#create_or_update
+L_all_done
