eb0f9f1848982e89c7a3a9f1e1388f4f417116a7
Daniel Wayne Armstrong <daniel@circuidipity.com>
2017-06-04 10:03:39 -0400

    grub[2]-install check

--- 02_grub_install.sh
+++ 02_grub_install.sh
@@ -12,12 +12,15 @@ INSTALL_OPTS="--target=i386-pc --force --recheck"
 BOOT_DIR="--boot-directory=$MNTPOINT/boot"
 DEVICE="/dev/$( echo "$USB_DEVICE" | cut -c -3 )"
 GRUB_CMD="$INSTALL_OPTS $BOOT_DIR $DEVICE"
-if [[ -x "grub-install" ]]; then
-    sudo grub-install $GRUB_CMD
+if [[ -x /usr/sbin/grub-install ]]; then
+    GRUB_INST="/usr/sbin/grub-install"
+elif [[ -x /usr/sbin/grub2-install ]]; then
+    GRUB_INST="/usr/sbin/grub2-install"
 else
-    L_echo_red "\n$( L_penguin ) .: grub-install command not found."
+    L_echo_red "\n$( L_penguin ) .: grub[2]-install command not found."
     exit 1
 fi
 echo "Install GRUB to the Master Boot Record (MBR) of $USB_DEVICE"
+sudo $GRUB_INST $GRUB_CMD
 L_sig_ok
 L_banner_end "GRUB INSTALL"
